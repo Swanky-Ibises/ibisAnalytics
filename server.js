fc14b81b5df409622b1088ba00978fb5fd0b2364
tyler <tyler@jackoldmacair.local>
2017-02-09 16:46:32 -0800

    add Access control headers

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ var fs = require('fs');
 //enable CORS
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 //set app to use body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
