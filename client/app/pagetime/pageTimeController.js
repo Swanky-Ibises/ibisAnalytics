5394e6bd2a8f9f65a2b59024c69ba8a5d666f774
SherryH <enjoy.wind7rain@gmail.com>
2017-02-13 10:32:31 -0800

    Add Country and City graphs from dummy data

--- client/app/pagetime/pageTimeController.js
+++ client/app/pagetime/pageTimeController.js
@@ -89,7 +89,42 @@ angular.module('pagetimeChartJs',['chartJsFactory','sharkanalytics.factory'])
         }
     };
   });
+})
+.controller('CountryDemographicsController', function($scope) {
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    $scope.series = ['Series A', 'Series B'];
 
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
 
+    $scope.options = {
+      title: {
+        display: true,
+        text: 'Independent Visit per Country '
+      },
+      layout:{
+        padding: 5
+      }
+    };
+})
+.controller('CityDemographicsController', function($scope) {
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    $scope.series = ['Series A', 'Series B'];
 
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+
+    $scope.options = {
+      title: {
+        display: true,
+        text: 'Independent Visit per City'
+      },
+      layout:{
+        padding: 5
+      }
+    };
 });
